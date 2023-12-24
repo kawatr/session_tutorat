5d09c1ebd8de623c02bb6b840f8a4c983bb3980d
berrada kawtar <kaoutarber2015@gmail.com>
2023-12-24 14:50:15 +0100

    authentification

---
+++ src/main/java/admin_user/gestion_tutorat.java
@@ -0,0 +1,13 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gestion_tutorat {
+
+	public static void main(String[] args) {
+		SpringApplication.run(gestion_tutorat.class, args);
+	}
+
+}
